a85ff51ed3e418e14c2990348769ed1719d830d7
Tech phoENIX <zaritskiy.kirill@gmail.com>
2023-02-17 11:40:02 +0200

    First version

--- PC L1/PC L1.cpp
+++ PC L1/PC L1.cpp
@@ -1,20 +1,138 @@
-// PC L1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <thread>
+#include <vector>
+#include <memory>
+#include <random>
+#include <chrono>
+#include <string>
+#include <fstream>
+using std::thread;
+using std::chrono::nanoseconds;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::cout;
 
-int main()
-{
+const unsigned int sizeOfArray = 10000;
+unsigned int numberOfThreads = 1;
+int minRandValue = 0, maxRandValue = 10;
+
+//target 
+int Array[sizeOfArray][sizeOfArray] = {};
+
+int intRand(const int& min, const int& max) {
+	static thread_local std::mt19937 generator;
+	std::uniform_int_distribution<int> distribution(min, max);
+	return distribution(generator);
+}
+
+void print(std::ofstream& file) {
+	for (size_t i = 0; i < sizeOfArray; i++)
+	{
+		for (size_t j = 0; j < sizeOfArray; j++)
+		{
+			file.width(5);
+			file << Array[i][j] << ' ';
+		}
+		file << '\n' << std::endl;
+	}
+
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void randomizeCellsEx(const unsigned int startPos, const unsigned int EndPos) {
+	for (size_t i = startPos; i <= EndPos; i++)
+	{
+		if (i / sizeOfArray != i % sizeOfArray) {
+			Array[i / sizeOfArray][i % sizeOfArray] = intRand(minRandValue, maxRandValue);
+		}
+	}
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void calculateCellsEx(const unsigned int startPos, const unsigned int EndPos) {
+	for (size_t i = startPos; i <= EndPos; i++)
+	{
+		for (size_t j = 0; j < sizeOfArray; j++)
+		{
+			if (j != i) {
+				Array[i][i] += Array[i][j];
+			}
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	std::vector<std::string> params;
+
+	for (int i = 1; i < argc; i++)
+	{
+		params.push_back(std::string(argv[i]));
+	}
+
+	switch (params.size()) 
+	{
+	case 3:
+		maxRandValue = std::stoi(params[2]);
+	case 2:
+		minRandValue = std::stoi(params[1]);
+	case 1:
+		numberOfThreads = std::stoi(params[0]);
+	}
+
+	const float randOpPerThread = (float)sizeOfArray * sizeOfArray / numberOfThreads,
+		calcOpPerThread = (float)sizeOfArray / numberOfThreads;
+
+	cout << "Starting parameters:" <<
+		"\n Matrix height/width: " << sizeOfArray <<
+		"\n Number of threads: " << numberOfThreads <<
+		"\n min random value: " << minRandValue <<
+		"\n max random value: " << maxRandValue <<
+		"\n Random operations per thread: " << randOpPerThread <<
+		"\n Calculations per thread: " << calcOpPerThread << std::endl;
+
+	std::ofstream matrixSave("PCL1.log");
+
+
+	std::vector<std::thread> threads;
+	
+	auto startTime = high_resolution_clock::now();
+	for (size_t i = 0; i < numberOfThreads; i++)
+	{
+		unsigned int start = i * randOpPerThread;
+		unsigned int end = (i + 1) * randOpPerThread-1;
+		threads.push_back(thread(randomizeCellsEx, start, end));
+	}
+	for (thread& th : threads)
+	{
+		th.join();
+	}
+	auto endTime = high_resolution_clock::now();
+	auto elapsed = duration_cast<nanoseconds>(endTime - startTime);
+
+	threads.clear();
+	cout << "Randomized in " << (elapsed.count() * 1e-9) << " s\n" << std::endl;
+	/*print(matrixSave);
+	cout << "Logged" << std::endl;*/
+
+	startTime = high_resolution_clock::now();
+	for (size_t i = 0; i < numberOfThreads; i++)
+	{
+		unsigned int start = i * calcOpPerThread;
+		unsigned int end = (i + 1) * calcOpPerThread - 1;
+		threads.push_back(thread(calculateCellsEx, start, end));
+	}
+	for (thread& th : threads)
+	{
+		th.join();
+	}
+	endTime = high_resolution_clock::now();
+	auto elapsed2 = duration_cast<nanoseconds>(endTime - startTime);
+
+	threads.clear();
+	cout << "Calculated in " << elapsed2.count() * 1e-9 << " s\n";
+	cout << "total: " << (elapsed.count() + elapsed2.count()) * 1e-9 << " s\n" << std::endl;
+
+	/*print(matrixSave);
+	cout << "Logged" << std::endl;*/
+	matrixSave.close();
+}
